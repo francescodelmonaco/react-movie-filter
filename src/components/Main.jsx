78bbc7f75075dd3bac71e0599a5438ed7587435f
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-02-25 17:49:40 +0100

    add: genre filter

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,22 +1,49 @@
+import { useState, useEffect } from "react";
+
+// array di film
+const films = [
+    { title: 'Inception', genre: 'Fantascienza' },
+    { title: 'Il Padrino', genre: 'Thriller' },
+    { title: 'Titanic', genre: 'Romantico' },
+    { title: 'Batman', genre: 'Azione' },
+    { title: 'Interstellar', genre: 'Fantascienza' },
+    { title: 'Pulp Fiction', genre: 'Thriller' },
+];
+
 const Main = () => {
-    // array di film
-    const filmsArray = [
-        { title: 'Inception', genre: 'Fantascienza' },
-        { title: 'Il Padrino', genre: 'Thriller' },
-        { title: 'Titanic', genre: 'Romantico' },
-        { title: 'Batman', genre: 'Azione' },
-        { title: 'Interstellar', genre: 'Fantascienza' },
-        { title: 'Pulp Fiction', genre: 'Thriller' },
-    ];
+    // filtro per genere
+    const [selected, setSelected] = useState("");
+    const [filtered, setFiltered] = useState(films);
+
+    useEffect(() => {
+        setFiltered((curr) => {
+            if (selected === "") {
+                return films;
+            } else {
+                return films.filter(film => film.genre === selected)
+            }
+        })
+    }, [selected]);
+
+    // visualizzare lista film per genere selezionato
+    const handleGenre = (e) => {
+        setSelected(e.target.value);
+    };
 
     return (
         <main>
             <div className="margin-60">
                 <div className="genre-selector">
                     <h2>Seleziona il genere:</h2>
-                    <select name="genre-list" id="films">
+                    <select
+                        name="genre-list"
+                        id="films"
+                        value={selected}
+                        onChange={handleGenre}
+                    >
+                        <option value=""></option>
                         {
-                            filmsArray.map((film) => (
+                            filtered.map((film) => (
                                 <option value={film.genre}>{film.genre}</option>
                             ))
                         }
@@ -25,7 +52,7 @@ const Main = () => {
 
                 <ul>
                     {
-                        filmsArray.map((film, id) => (
+                        filtered.map((film, id) => (
                             <div className="list-row">
                                 <li key={id} >{film.title}</li>
                             </div>
